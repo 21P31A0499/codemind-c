3098bd9714971cea93cf8321b22d02810ab3e160
21P31A0499 <99956298+21P31A0499@users.noreply.github.com>
2022-02-19 17:09:15 +0530

    #include<stdio.h> int main() {     int num,remider,largest=0;     //print("Enter the Number :");     scanf("%d",&num);
        while(num>0)
        {
            remider=num%10;
            if(largest<remider)
            {
                largest=remider;
            }
            num=num/10;
        }
        printf("%d",largest);
        return 0;
    }

---
+++ Largest_Digit.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int num,remider,largest=0;
+    //print("Enter the Number :");
+    scanf("%d",&num);
+    
+    while(num>0)
+    {
+        remider=num%10;
+        if(largest<remider)
+        {
+            largest=remider;
+        }
+        num=num/10;
+    }
+    printf("%d",largest);
+    return 0;
+}
